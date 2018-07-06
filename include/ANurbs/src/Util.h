be46fbe0854adbcbedff3b38d0b73c2c534811af
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-07-06 21:30:58 +0200

    Fix typename

--- include/ANurbs/src/Util.h
+++ include/ANurbs/src/Util.h
@@ -29,7 +29,7 @@ struct Weights<std::vector<TWeight>>
 template <typename TContainer>
 struct Weights
 {
-    using Type = std::result_of_t<TContainer(std::size_t)>;
+    using Type = typename std::result_of<TContainer(std::size_t)>::type;
     using ContainerType = TContainer;
 
     static Type
