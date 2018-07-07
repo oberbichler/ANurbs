8aeeee23c86b66f56b297b6592d155796a604252
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-07-07 10:57:51 +0200

    Add SurfaceBase

---
+++ include/ANurbs/src/SurfaceBase.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "Interval.h"
+#include "VectorMath.h"
+
+#include <vector>
+
+namespace ANurbs {
+
+template <typename TScalar, typename TVector>
+class SurfaceBase
+{
+public:
+    using SurfaceBaseType = SurfaceBase<TScalar, TVector>;
+
+    using ScalarType = TScalar;
+    using VectorType = TVector;
+    using IntervalType = Interval<ScalarType>;
+
+public:
+    static constexpr int
+    Dimension(
+    )
+    {
+        return VectorMath<VectorType>::Dimension();
+    }
+
+    virtual IntervalType
+    DomainU(
+    ) const = 0;
+    
+    virtual IntervalType
+    DomainV(
+    ) const = 0;
+
+    virtual VectorType
+    PointAt(
+        const ScalarType& u,
+        const ScalarType& v
+    ) const = 0;
+    
+    virtual std::vector<VectorType>
+    DerivativesAt(
+        const ScalarType& u,
+        const ScalarType& v,
+        const int& order
+    ) const = 0;
+};
+
+} // namespace ANurbs
