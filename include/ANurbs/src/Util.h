370a95874a4178f2daf938152a1a800f31308e71
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-07-07 09:34:00 +0200

    Add SurfaceWeights

--- include/ANurbs/src/Util.h
+++ include/ANurbs/src/Util.h
@@ -42,5 +42,27 @@ struct CurveWeights
     }
 };
 
+
+template <typename TContainer>
+struct SurfaceWeights;
+
+template <typename TContainer>
+struct SurfaceWeights
+{
+    using Type =
+        typename std::result_of<TContainer(int, int)>::type;
+    using ContainerType = TContainer;
+
+    static Type
+    Get(
+        const ContainerType& container,
+        const int& indexU,
+        const int& indexV
+    )
+    {
+        return container(indexU, indexV);
+    }
+};
+
 } // Util
 } // ANurbs
