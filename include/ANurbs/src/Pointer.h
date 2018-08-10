155193b02ccd5930561a74c28751019495db0573
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2018-08-10 19:51:30 +0200

    Add `Unique` pointer alias

--- include/ANurbs/src/Pointer.h
+++ include/ANurbs/src/Pointer.h
@@ -7,12 +7,15 @@ namespace ANurbs {
 template <typename T>
 using Pointer = std::shared_ptr<T>;
 
+template <typename T>
+using Unique = std::unique_ptr<T>;
+
 template <typename T, typename... TArgs>
-std::unique_ptr<T>
+Unique<T>
 New(
     TArgs&&... args)
 {
-    return std::unique_ptr<T>(new T(std::forward<TArgs>(args)...));
+    return Unique<T>(new T(std::forward<TArgs>(args)...));
 }
 
 } // namespace ANurbs
