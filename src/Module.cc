ad0a144a560af43694d2d5c4ea6b7b00852a11d1
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-05-22 17:49:49 +0200

    Remove unused headers

--- src/Module.cc
+++ src/Module.cc
@@ -1,11 +1,7 @@
 #include <ANurbs/ANurbs.h>
 
-#include <pybind11/pybind11.h>
 #include <pybind11/eigen.h>
-#include <pybind11/functional.h>
-#include <pybind11/stl.h>
 #include <pybind11/stl_bind.h>
-
 
 PYBIND11_MODULE(ANurbs, m) {
     m.doc() = "A simple NURBS library for Python";
