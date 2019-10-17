73e57ff19a0462fc715f530b097c8859bf54dd61
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-10-17 16:52:05 +0200

    Drop unused imports

--- tests/TestHilbertCurve.py
+++ tests/TestHilbertCurve.py
@@ -1,7 +1,6 @@
 import unittest
 import ANurbs as an
-import numpy as np
-from numpy.testing import assert_array_almost_equal, assert_almost_equal, assert_array_equal
+from numpy.testing import assert_array_almost_equal, assert_array_equal
 
 class TestHilbertCurve(unittest.TestCase):
     def test_rotl_2d(self):
--- tests/TestRTree.py
+++ tests/TestRTree.py
@@ -1,7 +1,7 @@
 import unittest
 import ANurbs as an
 import numpy as np
-from numpy.testing import assert_array_almost_equal, assert_almost_equal, assert_equal, assert_array_equal
+from numpy.testing import assert_array_almost_equal, assert_equal, assert_array_equal
 
 class TestRTree(unittest.TestCase):
     def test_example_2d(self):
