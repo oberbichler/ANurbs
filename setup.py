f74b4df5d582b6ac871458a56bd003435332055a
thomas <thomas.oberbichler@gmail.com>
2019-10-28 19:02:27 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-ANURBS_VERSION = '0.4.1'
+ANURBS_VERSION = '0.4.2'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
