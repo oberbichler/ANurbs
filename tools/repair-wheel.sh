8c00197160418089a2831952a1d4cc02fe2015b9
thomas <thomas.oberbichler@gmail.com>
2019-10-28 18:54:28 +0100

    Create repair-wheel.sh

---
+++ tools/repair-wheel.sh
@@ -0,0 +1,6 @@
+set -e
+yum -y remove cmake
+/opt/python/${PYTHON_VERSION}/bin/pip install cmake
+export PATH=$PATH:/opt/python/${PYTHON_VERSION}/bin/
+/opt/python/${PYTHON_VERSION}/bin/pip wheel .
+auditwheel repair hyperjet-*-${PYTHON_VERSION}-*.whl
