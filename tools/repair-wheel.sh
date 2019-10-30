10a5cbe933e328fa567ee3ab9769c8d499c15feb
thomas <thomas.oberbichler@gmail.com>
2019-10-30 08:55:01 +0100

    Update repair-wheel.sh

--- tools/repair-wheel.sh
+++ tools/repair-wheel.sh
@@ -2,5 +2,5 @@ set -e
 yum -y remove cmake
 /opt/python/${PYTHON_VERSION}/bin/pip install cmake
 export PATH=$PATH:/opt/python/${PYTHON_VERSION}/bin/
-/opt/python/${PYTHON_VERSION}/bin/pip wheel .
+/opt/python/${PYTHON_VERSION}/bin/python setup.py bdist_wheel
 auditwheel repair ANurbs-*-${PYTHON_VERSION}-*.whl
