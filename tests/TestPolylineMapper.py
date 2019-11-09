93f593bdc9b01a56aaab2ab6a79ca4aa1ce5d7b6
Thomas Oberbichler <thomas.oberbichler@gmail.com>
2019-11-09 14:44:25 +0100

    Test PolylineMapper

---
+++ tests/TestPolylineMapper.py
@@ -0,0 +1,20 @@
+import unittest
+import anurbs as an
+from numpy.testing import assert_equal
+
+class TestPolylineMapper(unittest.TestCase):
+    def test_map_2d(self):
+        polyline = [[0, 0], [1, 1], [2, 0]]
+
+        mapper = an.PolylineMapper2D(polyline)
+
+        t0, i0, t1, i1 = mapper.map([0.25, 0.25], tolerance=1e-4)
+
+        assert_equal([t0, i0, t1, i1], [0.75, 0, 0.25, 1])
+
+        t0, i0, t1, i1 = mapper.map([1.75, 0.25], tolerance=1e-4)
+
+        assert_equal([t0, i0, t1, i1], [0.25, 1, 0.75, 2])
+
+if __name__ == '__main__':
+    unittest.main()
