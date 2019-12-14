6523d31e3f2746faf0e4a5990d8d951b889fb5a0
thomas <thomas.oberbichler@gmail.com>
2019-12-14 10:59:41 +0100

    Delete repair-wheel.sh

--- tools/repair-wheel.sh
+++
@@ -1,6 +0,0 @@
-set -e
-yum -y remove cmake
-/opt/python/${PYTHON_VERSION}/bin/pip install cmake
-export PATH=$PATH:/opt/python/${PYTHON_VERSION}/bin/
-/opt/python/${PYTHON_VERSION}/bin/pip wheel .
-auditwheel repair anurbs-*-${PYTHON_VERSION}-*.whl
