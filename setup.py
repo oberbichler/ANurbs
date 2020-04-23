be6ee1c430124742fbd8d373895a4297fa7a9720
thomas <thomas.oberbichler@gmail.com>
2020-04-23 14:42:43 +0200

    Increase version number

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension
 from setuptools.command.build_ext import build_ext
 from distutils.version import LooseVersion
 
-ANURBS_VERSION = '0.9.0'
+ANURBS_VERSION = '0.9.1'
 
 class CMakeExtension(Extension):
     def __init__(self, name, sourcedir=''):
